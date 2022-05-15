0108f1ba08b6d1879c6cb8224050962911fb3674
Kevin Alvarez <kalvarez@unsa.edu.pe>
2022-05-15 12:24:00 -0500

    Creando index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+const fs = require('fs')
+const path = require('path')
+const express = require('express')
+const bp = require('body-parser')
+const MarkdownIt = require('markdown-it'),
+    md = new MarkdownIt();//servira para traducir markdown a html
+const app = express()
+
+app.use(express.static('pub'))
+app.use(bp.json())
+app.use(bp.urlencoded({
+    extended: true
+}))
+
+app.listen(3000, () => {
+    console.log("Escuchando en: http://localhost:3000")
+})
+
+app.get('/', (request, response) => {
+    response.sendFile(path.resolve(__dirname, 'index.html'))
+})
