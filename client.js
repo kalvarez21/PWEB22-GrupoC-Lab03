508ffeaa3a43d194a978af79d702d33800465260
Kevin Alvarez <kalvarez@unsa.edu.pe>
2022-05-15 12:47:05 -0500

    funcion crearArchivo prueba01

--- client.js
+++ client.js
@@ -1,5 +1,12 @@
 function nuevoArchivo(){
-  let html = '<textarea id="editarTexto" rows="8" cols="80"></textarea><br>'+
+  let html = '<input type="text" id="nuevoTitulo" placeholder="Ingrese un titulo para el texto">' +
+             '<textarea id="editarTexto" rows="8" cols="80"></textarea><br>'+
              '<input type="button" id="crearArchivo" value="Crear Archivo" onclick="crearArchivo()">'
   document.querySelector(".main").innerHTML= html;
+}
+function crearArchivo(){
+  const nombreArchivo = document.querySelector("#nuevoTitulo")
+  const contenidoArchivo = document.querySelector("#editarTexto")
+  console.log(nombreArchivo)
+  console.log(contenidoArchivo)
 }
